35845b75485744882f3248dcb7dff376036b9a9c
Kavi <kaviwijewardhana@gmail.com>
2026-04-18 02:28:24 +0530

    Update StudentBookingFormPage.jsx

--- frontend/src/pages/StudentBookingFormPage.jsx
+++ frontend/src/pages/StudentBookingFormPage.jsx
@@ -216,7 +216,7 @@ export default function StudentBookingFormPage() {
     }
 
     if (form.endTime <= form.startTime) {
-      setError('End time must be after start time.');
+      setError('Entime must be after start time.');
       return;
     }
 
